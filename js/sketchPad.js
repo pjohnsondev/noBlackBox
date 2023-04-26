2f918c165123ee657408ded0dd680f65d371dbcc
Paul <pjohnsondev@gmail.com>
2023-04-26 15:32:30 +0900

    add drawing functionality

--- js/sketchPad.js
+++ js/sketchPad.js
@@ -4,9 +4,49 @@ class SketchPad{
         this.canvas.width=size;
         this.canvas.height=size;
         this.canvas.style=`
-            background-color:white;
+            background-color: white;
             box-shaddow: 0px 0px 10px 2px black;
         `;
         containter.appendChild(this.canvas);
+
+        this.ctx=this.canvas.getContext("2d");
+
+        this.paths=[];
+        this.isDrawing = false;
+
+        this.#addEventListeners();
+    }
+
+    #addEventListeners(){
+        this.canvas.onmousedown=(evt)=>{
+            const mouse = this.#getMouse(evt)
+            this.paths.push([mouse]);
+            this.isDrawing=true;
+        }
+        this.canvas.onmousemove=(evt)=>{
+            if(this.isDrawing){
+                const mouse = this.#getMouse(evt);
+                const lastPath=this.paths[this.paths.length-1];
+                lastPath.push(mouse);
+                this.#redraw();
+            }
+        }
+        this.canvas.onmouseup=()=>{
+            this.isDrawing=false;
+        }
+    }
+
+    #redraw(){
+        this.ctx.clearRect(0,0,
+            this.canvas.width, this.canvas.height);
+        draw.paths(this.ctx,this.paths);
+    }
+
+    #getMouse=(evt)=>{
+        const rect=this.canvas.getBoundingClientRect();
+        return [
+            Math.round(evt.clientX-rect.left),
+            Math.round(evt.clientY-rect.top)
+        ];
     }
 }
